a2d7afcf07333c752e3b9a4f6bb8eb67f9f3ef1c
ananchik <safronov.thief@yandex.ru>
2023-09-22 10:25:03 +0300

    Add files

---
+++ constructor/frontend/src/pages/ScoringPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const ScoringPage = () => {
+    return (
+        <div>
+            <h1 style={{ color: "black" }}>
+                Страницы скорринга моделей
+            </h1>
+            <h2>
+                Страница в разработке
+            </h2>
+        </div>
+    );
+}
+
+export default ScoringPage;
