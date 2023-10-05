c2f2e21631d2057ebbef644696d1863313b425a3
Сафронов Денис Алексеевич <d.safronov@nmkn.bmstu.ru>
2023-10-05 15:50:37 +0300

    Add edit function on ScoringPage

--- constructor/frontend/src/components/AppRouter.jsx
+++ constructor/frontend/src/components/AppRouter.jsx
@@ -1,30 +1,28 @@
-import React from 'react';
-import { Navigate, Route, Routes } from 'react-router-dom';
-import ScoringPage from '../pages/ScoringPage';
-import Upload from '../pages/UploadPage';
-import PipelinePage from '../pages/PipelinePage';
-import CrmPage from '../pages/CrmPage';
-import Error from '../pages/Error';
-import LoginPage from '../pages/LoginPage';
-import Logout from '../pages/LogoutPage';
+import React from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import ScoringPage from "../pages/ScoringPage";
+import Upload from "../pages/UploadPage";
+import PipelinePage from "../pages/PipelinePage";
+import CrmPage from "../pages/CrmPage";
+import Error from "../pages/Error";
+import LoginPage from "../pages/LoginPage";
+import Logout from "../pages/LogoutPage";
 
 const AppRouter = () => {
-    return (
-        <Routes>
-            <Route path="/" element={<Navigate to="/login" replace />} />
-            <Route path="/scoring" element={<ScoringPage />} />
-            <Route path="/upload" element={<Upload />} />
-            <Route path="/pipeline" element={<PipelinePage />} />
-            <Route path="/crm" element={<CrmPage />} />
-            <Route path="/error" element={<Error />} />
-            <Route path="/login" element={<LoginPage />}/> 
-            < Route path="/logout" element={<Logout />}/>
-            <Route
-                path="*"
-                element={<Navigate to="/error" replace />}
-            />
-        </Routes>
-    );
-}
+
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/upload" replace />} />
+      <Route path="/scoring" element={<ScoringPage />} />
+      <Route path="/upload" element={<Upload />} />
+      <Route path="/pipeline" element={<PipelinePage />} />
+      <Route path="/crm" element={<CrmPage />} />
+      <Route path="/error" element={<Error />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/logout" element={<Logout />} />
+      <Route path="*" element={<Navigate to="/error" replace />} />
+    </Routes>
+  );
+};
 
 export default AppRouter;
--- constructor/frontend/src/pages/UploadPage.jsx
+++ constructor/frontend/src/pages/UploadPage.jsx
@@ -34,3 +34,36 @@ function UploadPage() {
 
 
 export default UploadPage;
+
+
+// import React, {useEffect, useState} from 'react';
+// import ContentGroup from '../components/UploadPage/ContentGroup/ContentGroup.jsx';
+// import 'bootstrap/dist/css/bootstrap.css';
+// import '../styles/App.css';
+// import '../styles/App.css';
+// import UploadBlock from '../components/UploadPage/UploadBlock/UploadBlock.jsx';
+
+// function UploadPage() {
+//     const [uploadedFiles, setUploadedFiles] = useState([{name: 123}])
+
+//     const uploadHandler = (item) => {
+//         setUploadedFiles([...uploadedFiles, item])
+//     }
+
+//     return (
+//         <div className="UploadPage">
+//                 <div className="d-flex justify-content-center flex-row">
+//                     <div className="upload-block mx-3 p-4">
+//                         <h4 className='text-center m-2'>Добавить файлы</h4>
+//                         {/* Поле загрузки данных */}
+//                         <div>
+//                             <UploadBlock uploadedFiles={uploadedFiles} setUploadedFiles={uploadHandler} />
+//                         </div>
+//                     </div>
+//                     <ContentGroup uploadedFiles={uploadedFiles} setUploadedFiles={setUploadedFiles} />
+//                 </div>
+//         </div>
+//     );
+// }
+
+// export default UploadPage;
