116d7941e0e4d4cd285fd51ba43b2396700e1ac2
Grigory <grisha14423@gmail.com>
2023-10-19 20:16:33 +0300

    try to post upload data

--- constructor/frontend/src/components/UploadPage/UploadBlock/UploadBlock.tsx
+++ constructor/frontend/src/components/UploadPage/UploadBlock/UploadBlock.tsx
@@ -1,41 +1,49 @@
-import '../../../index.css';
-import { InboxOutlined } from '@ant-design/icons';
-import type { UploadProps } from 'antd';
-import { message, Upload } from 'antd';
-import React from "react";
+import "../../../index.css"
+import { InboxOutlined } from "@ant-design/icons"
+import type { UploadProps } from "antd"
+import { message, Upload } from "antd"
+import axios from "axios"
+import React from "react"
 
-const { Dragger } = Upload;
+const { Dragger } = Upload
 
 const props: UploadProps = {
-  name: 'filename', // это название колонки в БД
+  name: "filename", // это название колонки в БД
   multiple: true,
-  action: 'http://127.0.0.1:8000/api/attributes/', // путь куда передавать данные
+  action: "http://127.0.0.1:8000/api/attributes/", // путь куда передавать данные
   onChange(info) {
-    const { status } = info.file;
-    if (status !== 'uploading') {
-      console.log(info.file, info.fileList);
+    const { status } = info.file
+    if (status !== "uploading") {
+      console.log("vot", info.file, info.fileList)
+      axios
+        .post("http://127.0.0.1:8000/api/files/", info.file)
+        .then(function (response) {
+          console.log(response)
+        })
+        .catch(function (error) {
+          console.log(error)
+        })
     }
-    if (status === 'done') {
-      message.success(`${info.file.name} file uploaded successfully.`);
-    } else if (status === 'error') {
-      message.error(`${info.file.name} file upload failed.`);
+    if (status === "done") {
+      message.success(`${info.file.name} file uploaded successfully.`)
+    } else if (status === "error") {
+      message.error(`${info.file.name} file upload failed.`)
     }
   },
   onDrop(e) {
-    console.log('Dropped files', e.dataTransfer.files);
+    console.log("Dropped files", e.dataTransfer.files)
   },
-};
+}
 
 const OldUploadBlock: React.FC = () => (
   <Dragger {...props}>
     <p className="ant-upload-drag-icon">
       <InboxOutlined />
     </p>
-    <p className="ant-upload-text">Нажмите или перенесите файл в эту область для загрузки</p>
+    <p className="ant-upload-text">
+      Нажмите или перенесите файл в эту область для загрузки
+    </p>
   </Dragger>
-);
+)
 
-export default OldUploadBlock;
-
-
-
+export default OldUploadBlock
