4dc4626d111c8ce25a80505667fd1b14df4d91e5
Grigory <grisha14423@gmail.com>
2023-10-25 10:10:07 +0300

    add a few fields

--- constructor/frontend/src/pages/NewClientPage.jsx
+++ constructor/frontend/src/pages/NewClientPage.jsx
@@ -6,7 +6,6 @@ import { Link, useParams } from "react-router-dom"
 import axios from "axios"
 import { validator } from "../components/utils/validator"
 import SelectSearchField from "../components/CrmPage/Form/SelectSearchField"
-import Select from "react-select"
 
 const NewClientPage = () => {
   const params = useParams()
@@ -66,12 +65,67 @@ const NewClientPage = () => {
   }, [])
 
   const validatorConfig = {
-    test: {
+    // test: {
+    //   isRequired: {
+    //     message: "Это поле обязательно для заполнения",
+    //   },
+    // },
+
+    status: {
       isRequired: {
         message: "Это поле обязательно для заполнения",
       },
     },
-    risk: {
+
+    category: {
+      isRequired: {
+        message: "Это поле обязательно для заполнения",
+      },
+    },
+
+    stage: {
+      isRequired: {
+        message: "Это поле обязательно для заполнения",
+      },
+    },
+
+    supprot: {
+      isRequired: {
+        message: "Это поле обязательно для заполнения",
+      },
+    },
+
+    regions: {
+      isRequired: {
+        message: "Это поле обязательно для заполнения",
+      },
+    },
+    name: {
+      isRequired: {
+        message: "Это поле обязательно для заполнения",
+      },
+    },
+    INN: {
+      isRequired: {
+        message: "Это поле обязательно для заполнения",
+      },
+    },
+    date: {
+      isRequired: {
+        message: "Это поле обязательно для заполнения",
+      },
+    },
+    date2: {
+      isRequired: {
+        message: "Это поле обязательно для заполнения",
+      },
+    },
+    sum: {
+      isRequired: {
+        message: "Это поле обязательно для заполнения",
+      },
+    },
+    descr: {
       isRequired: {
         message: "Это поле обязательно для заполнения",
       },
@@ -84,6 +138,23 @@ const NewClientPage = () => {
   const [testData, setTestData] = useState({
     test: "",
     risk: "",
+    sources: "",
+    managers: "",
+    status: "",
+    type: "",
+    category: "",
+    positive: "",
+    stage: "",
+    negative: "",
+    support: "",
+    PRD: "",
+    regions: "",
+    name: "",
+    INN: "",
+    date: "",
+    date2: "",
+    sum: "",
+    descr: "",
   })
   const [testApi, setTestApi] = useState()
 
@@ -100,6 +171,97 @@ const NewClientPage = () => {
     { label: "Высокий риск", value: "Высокий риск", name: "risk" },
     { label: "Средний риск", value: "Средний риск", name: "risk" },
     { label: "Низкий риск", value: "Низкий риск", name: "risk" },
+  ]
+
+  const sources = [
+    { label: "ТНО, ФНС    ", value: "ТНО, ФНС    ", name: "sources" },
+    {
+      label: "Входящие звонки    ",
+      value: "Входящие звонки    ",
+      name: "sources",
+    },
+    { label: "РГ    ", value: "РГ    ", name: "sources" },
+  ]
+  const menegers = [
+    { label: "Бабасов П.Н.", value: "Бабасов П.Н.", name: "menegers" },
+    {
+      label: "Бойченко И.А.    ",
+      value: "Бойченко И.А.    ",
+      name: "menegers",
+    },
+    {
+      label: "Емельянова Е.А.    ",
+      value: "Емельянова Е.А.    ",
+      name: "menegers",
+    },
+  ]
+  const status = [
+    { label: "должник    ", value: "должник    ", name: "status" },
+    { label: "кредитор    ", value: "кредитор    ", name: "status" },
+  ]
+
+  const category = [
+    {
+      label: "системообразующее    ",
+      value: "системообразующее    ",
+      name: "category",
+    },
+    {
+      label: "градообразующее    ",
+      value: "градообразующее    ",
+      name: "category",
+    },
+  ]
+
+  const stage = [
+    {
+      label: "Получено обращение     ",
+      value: "Получено обращение     ",
+      name: "stage",
+    },
+    {
+      label: "Проведено первое совещание     ",
+      value: "Проведено первое совещание     ",
+      name: "stage",
+    },
+    {
+      label: "Истребованы документы",
+      value: "Истребованы документы",
+      name: "stage",
+    },
+  ]
+
+  const support = [
+    {
+      label: "отсрочка / рассрочка (ст. 64 НК РФ)    ",
+      value: "отсрочка / рассрочка (ст. 64 НК РФ)    ",
+      name: "support",
+    },
+    {
+      label: "отлагательные меры    ",
+      value: "отлагательные меры    ",
+      name: "support",
+    },
+    {
+      label: "отлагательные меры + МС    ",
+      value: "отлагательные меры + МС",
+      name: "support",
+    },
+  ]
+
+  const regions = [
+    {
+      number: 1,
+      label: "1 Республика Адыгея",
+      value: "1 Республика Адыгея",
+      name: "regions",
+    },
+    {
+      number: 2,
+      label: " 2 Республика Башкортостан",
+      value: " 2 Республика Башкортостан",
+      name: "regions",
+    },
   ]
 
   useEffect(() => {
@@ -138,21 +300,119 @@ const NewClientPage = () => {
                 <h3 className="text-center mt-2">Данные клиента</h3>
                 <Divider />
               </div>
-              <SelectSearchField
+              {/* <SelectSearchField
                 options={riskList}
                 onChange={handleChangeTest}
                 name="risk"
                 error={errors.risk}
                 label="New label select"
                 placeholder={testData.risk}
+              /> */}
+              <SelectSearchField
+                options={riskList}
+                onChange={handleChangeTest}
+                name="risk"
+                error={errors.risk}
+                label="Платежеспособность"
+                placeholder={testData.risk}
+              />
+              <SelectSearchField
+                options={regions}
+                onChange={handleChangeTest}
+                name="regions"
+                error={errors.regions}
+                label="Регион"
+                placeholder={testData.regions}
+              />
+              <SelectSearchField
+                options={status}
+                onChange={handleChangeTest}
+                name="status"
+                error={errors.status}
+                label="Статус заявителя"
+                placeholder={testData.status}
+              />
+              <SelectSearchField
+                options={category}
+                onChange={handleChangeTest}
+                name="category"
+                error={errors.category}
+                label="Категория"
+                placeholder={testData.category}
+              />
+              <SelectSearchField
+                options={support}
+                onChange={handleChangeTest}
+                name="support"
+                error={errors.support}
+                label="Мера поддержки запрашиваемая"
+                placeholder={testData.support}
+              />
+              <SelectSearchField
+                options={sources}
+                onChange={handleChangeTest}
+                name="sources"
+                error={errors.sources}
+                label="Источник информации"
+                placeholder={testData.sources}
+              />
+              <SelectSearchField
+                options={stage}
+                onChange={handleChangeTest}
+                name="stage"
+                error={errors.stage}
+                label="Стадия рассмотрения"
+                placeholder={testData.stage}
               />
 
-              <TextField
+              {/* <TextField
                 label="label tex"
                 name="test"
                 value={testData.test}
                 onChange={handleChangeTest}
                 error={errors.test}
+              /> */}
+              <TextField
+                label="Нименование клиента"
+                name="name"
+                value={testData.name}
+                onChange={handleChangeTest}
+                error={errors.name}
+              />
+              <TextField
+                label="ИНН"
+                name="INN"
+                value={testData.INN}
+                onChange={handleChangeTest}
+                error={errors.INN}
+              />
+              <TextField
+                label="Сумма задолженности"
+                name="sum"
+                value={testData.sum}
+                onChange={handleChangeTest}
+                error={errors.sum}
+              />
+              <TextField
+                label="Описание события"
+                name="descr"
+                value={testData.descr}
+                onChange={handleChangeTest}
+                error={errors.descr}
+              />
+              <TextField
+                label="Дата обращения в МИДУОЛ"
+                name="date"
+                value={testData.date}
+                onChange={handleChangeTest}
+                error={errors.date}
+              />
+              <TextField
+                label="Дата наступления события"
+                name="date2"
+                value={testData.date2}
+                onChange={handleChangeTest}
+                error={errors.date2}
               />
               <div className="row row-centered  colored">
                 <button
